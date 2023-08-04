319eef23508fdfb1f91c5f9bc55bcf83e2d02dc2
Bashaer <110439978+bashaer310@users.noreply.github.com>
2023-08-04 20:43:23 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,24 @@ public class Main {
         /*Circle[] Circle={new Circle(4),new Circle(2),new Circle(1)};
         for (int i = 0; i < Circle.length ; i++) {
             System.out.println(Circle[i].toString());
-            System.out.println("The area of circle "+(i + 1)+" is "+Circle[i].getArea());
-            System.out.println("The perimeter of circle "+(i + 1)+" is "+Circle[i].getPerimeter());
+            System.out.println("The area of the circle "+(i + 1)+" is "+Circle[i].getArea());
+            System.out.println("The perimeter the of circle "+(i + 1)+" is "+Circle[i].getPerimeter());
         }*/
 
         //for Rectangle claas
         /*Rectangle[] Rectangle={new Rectangle(4,2),new Rectangle(3,1),new Rectangle(6,2)};
         for (int i = 0; i < Rectangle.length ; i++) {
             System.out.println(Rectangle[i].toString());
-            System.out.println("The area of rectangle " + (i + 1) + " is " + Rectangle[i].getArea());
-            System.out.println("The perimeter of rectangle " + (i + 1) + " is " + Rectangle[i].getPerimeter());
+            System.out.println("The area of the rectangle " + (i + 1) + " is " + Rectangle[i].getArea());
+            System.out.println("The perimeter the of rectangle " + (i + 1) + " is " + Rectangle[i].getPerimeter());
         }*/
 
         //for Square claas
         /*Square[] Square={new Square(2), new Square(3), new Square(6)};
         for (int i = 0; i < Square.length ; i++) {
             System.out.println(Square[i].toString());
-            System.out.println("The area of square " + (i + 1) + " is " + Square[i].getArea());
-            System.out.println("The perimeter of square " + (i + 1) + " is " + Square[i].getPerimeter());
+            System.out.println("The area of the square " + (i + 1) + " is " + Square[i].getArea());
+            System.out.println("The perimeter of the square " + (i + 1) + " is " + Square[i].getPerimeter());
         }*/
 
     }
